e95d38f75d5be1f1f83548a203e74cb7c50fcb4c
Daniel <titawom@gmail.com>
2023-08-26 13:17:56 +0200

    docker-compose works

--- frontend/src/app/components/airport-selector/airport-selector.component.ts
+++ frontend/src/app/components/airport-selector/airport-selector.component.ts
@@ -75,7 +75,7 @@ export class AirportSelectorComponent {
         unixTimeStartDate = 1517227200;
         unixTimeEndDate = 1517230800;
         
-        return "http://localhost:8000/api/all/" + 
+        return "api/all/" + 
                     unixTimeStartDate + "/" +
                     unixTimeEndDate;
     }
